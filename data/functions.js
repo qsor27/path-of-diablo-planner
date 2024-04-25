e4cd0d9720945c2138c479ba263ceea089407ded
D <33237051+qordwasalreadytaken@users.noreply.github.com>
2024-04-25 14:02:31 -0400

    discharge

--- data/functions.js
+++ data/functions.js
@@ -2174,11 +2174,11 @@ function getCTCSkillData(name, lvl, group) {
 			result.fDamage_max = skill.data.values[1][lvl] * (1 + (0.08*skills[23].level)) * (1 + Math.min(1,(skills[30].level+skills[30].force_levels))*(~~skills[30].data.values[1][skills[30].level+skills[30].extra_levels])/100);
 		}
 	else if (name == "Discharge") {
-		lDamage_min = character.getSkillData(skill,lvl,1); 
-		lDamage_max = character.getSkillData(skill,lvl,2);
-		result.lDamage =	(0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2) * (1+c.lDamage/100))
+		result.lDamage_min = skill.data.values[1][lvl] + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2) * (1+c.lDamage/100)); 
+		result.lDamage_max = skill.data.values[2][lvl] + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2) * (1+c.lDamage/100));
+//		result.lDamage =	(0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2) * (1+c.lDamage/100))
 		if (character.class_name == "Sorceress") {
-			result.lDamage = ((1 + 0.03*sk[12].level + 0.03*sk[14].level + 0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)) * (1+c.lDamage/100)) 
+			result.lDamage = (1 + 0.03*sk[12].level + 0.03*sk[14].level )
 		
 		}
 	}

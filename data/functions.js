805cb3fc799666d691280630808918483d4c8552
D <33237051+qordwasalreadytaken@users.noreply.github.com>
2024-04-25 16:09:39 -0400

    discharge

--- data/functions.js
+++ data/functions.js
@@ -2175,8 +2175,8 @@ function getCTCSkillData(name, lvl, group) {
 		}
 	}
 	else if (name == "Discharge" && elem < 3 && elem > 0) {
-		result.lDamage *= ((skill.data.values[2][lvl]) + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2))); 
-		//result.fDamage_min = skill.data.values[0][lvl]; result.fDamage_max = skill.data.values[1][lvl]; result.ar_bonus = skill.data.values[4][lvl];
+		//result.lDamage *= ((skill.data.values[2][lvl]) + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2))); 
+		result.lDamage_min = skill.data.values[1][lvl]; 
 		//result.lDamage_min *= ((1 + 0.03*sk[12].level + 0.03*sk[14].level + 0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)) * (1+c.lDamage/100));
 		//result.lDamage_max *= ((skill.data.values[2][lvl]) + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2))); 
 		//result.lDamage_max = skill.data.values[2][lvl] //+ (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2));

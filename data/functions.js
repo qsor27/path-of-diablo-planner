f29f5d0e5d05e2e65cddf08bb5cc3c7ffddd5848
D <33237051+qordwasalreadytaken@users.noreply.github.com>
2024-04-25 15:08:04 -0400

    discharge

--- data/functions.js
+++ data/functions.js
@@ -2176,13 +2176,14 @@ function getCTCSkillData(name, lvl, group) {
 	}
 	else if (name == "Discharge") {
 		//result.fDamage_min = skill.data.values[0][lvl]; result.fDamage_max = skill.data.values[1][lvl]; result.ar_bonus = skill.data.values[4][lvl];
-		result.lDamage_min = skill.data.values[1][lvl]; result.lDamage_max = skill.data.values[2][lvl]; //+ (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)); 
+		result.lDamage_min = skill.data.values[1][lvl] + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)); 
+		result.lDamage_max = skill.data.values[2][lvl] + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)); 
 		//result.lDamage_max = skill.data.values[2][lvl] //+ (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2));
 //		result.lDamage =	(0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2) * (1+c.lDamage/100))
 		if (character.class_name == "Sorceress") {
-			result.lDamage_min = (1 + 0.03*sk[12].level + 0.03*sk[14].level);
-			result.lDamage_max = (1 + 0.03*sk[12].level + 0.03*sk[14].level);
-		}
+			result.lDamage_min = skill.data.values[1][lvl] + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)); 
+			result.lDamage_max = skill.data.values[2][lvl] + (0.01*Math.floor(((character.energy + character.all_attributes)*(1+character.max_energy/100))/2)); 
+			}
 	}
 
 	// Necromancer
